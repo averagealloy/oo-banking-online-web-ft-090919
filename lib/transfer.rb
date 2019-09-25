925b9bab1f0ef02ea5c161a9be72d2f6c365fdf6
Mike McTighe <m.mctighe10@gmail.com>
2019-09-25 19:21:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
     if !(@sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount)
-      @status = "re"
+      @status = "complete"
     else
       @status = "rejected"
       puts "Transaction rejected. Please check your account balance."
