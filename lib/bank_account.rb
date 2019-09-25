eaf6ae650e2e48179486061311c1a2a24e51dbdc
Mike McTighe <m.mctighe10@gmail.com>
2019-09-25 19:03:02 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,7 @@
 class BankAccount
    attr_reader :name
    attr_accessor :status, :balance
-   def initialize(name, balance = 1000, status = "open”)
+   def initialize(name, balance = 1000, status = "open")
        @name = name
        @balance = balance
        @status = status
