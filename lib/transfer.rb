d9a15d1c691a5303831a8dd867ccba229d55ede3
Mike McTighe <m.mctighe10@gmail.com>
2019-09-25 19:32:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,10 +13,9 @@ class Transfer
     @sender.valid? && @receiver.valid?
   end
   def execute_transaction
-    if @sender.balance > @amount
-       @status == "pending"
-    #  @sender.balance -= @amount
-    #  @receiver.balance += @amount
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
       @status = "complete"
     else
       @status = "rejected"
