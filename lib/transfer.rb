fe5a7a12fb34087e6f592fd386708dcd39857803
Mike McTighe <m.mctighe10@gmail.com>
2019-09-25 19:19:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @status = "complete "
+      @status = "complete"
 
   end
 end
