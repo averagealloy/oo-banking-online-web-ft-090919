011c93336258049ecca3b0d8ed41179796929401
Mike McTighe <m.mctighe10@gmail.com>
2019-09-25 19:48:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
     @receiver.balance += @amount
     @status = "complete"
   else
-    puts "Transaction rejected. Please check your account balance."
     @status = "rejected"
+    puts "Transaction rejected. Please check your account balance."
   end
 end
